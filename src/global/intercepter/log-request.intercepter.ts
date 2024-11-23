5cfba7a47eb51ce63538a2e7e834086dea6ab239
sunniesfish <hhch5611.jh@gmail.com>
2024-11-23 22:54:15 +0900

    log

--- src/global/intercepter/log-request.intercepter.ts
+++ src/global/intercepter/log-request.intercepter.ts
@@ -1,16 +1,46 @@
-// import {
-//   CallHandler,
-//   ExecutionContext,
-//   Injectable,
-//   NestInterceptor,
-// } from '@nestjs/common';
-// import { Observable } from 'rxjs';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
-// @Injectable()
-// export class LogRequestInterceptor implements NestInterceptor {
-//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//     const request = context.switchToHttp().getRequest();
+@Injectable()
+export class LogRequestInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LogRequestInterceptor.name);
 
-//     return next.handle(); // 요청 처리를 계속 진행
-//   }
-// }
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const { method, url, headers } = request;
+    const startTime = Date.now();
+
+    this.logger.log(
+      `Incoming Request: [${method}] ${url} - Headers: ${JSON.stringify(headers)}`,
+    );
+
+    return next.handle().pipe(
+      tap(() => {
+        const elapsedTime = Date.now() - startTime;
+
+        this.logger.log(
+          `Outgoing Response: [${method}] ${url} - ${elapsedTime}ms`,
+        );
+      }),
+      catchError((error) => {
+        const elapsedTime = Date.now() - startTime;
+
+        this.logger.error(
+          `Error Response: [${method}] ${url} - ${elapsedTime}ms - Error: ${
+            error.message
+          }`,
+          error.stack,
+        );
+
+        throw error;
+      }),
+    );
+  }
+}
